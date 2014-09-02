6be5e029b1e56dc9febab8ac02756e101803c45b
Park Loqi Tamaroon <loqi@cruzio.com>
2014-09-01 17:18:21 -0700

    (inc) periodic commit

--- server/api/spot/spot.spec.js
+++ server/api/spot/spot.spec.js
@@ -14,6 +14,7 @@ describe('GET /api/spots', function() {
       .end(function(err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
+        
         done();
       });
   });
