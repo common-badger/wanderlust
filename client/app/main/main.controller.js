3d69f67c190089c33411cc19534b7ea1dba1fac6
Dan Thareja <danthareja@gmail.com>
2014-09-03 17:02:42 -0400

    (feat) adds splash page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('wanderlustApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $state, $document) {
 
+    // Hacky way to only show background image on body initially
+    angular.element($document[0].body).css({
+      'overflow-y': 'hidden ! important',
+      'overflow-x': 'hidden ! important',
+      'background-image': "url('/assets/images/background.jpg')",
+      'background-size': 'cover',
+      'background-repeat': 'no-repeat'
+    });
+
+    $scope.navToToursByLocation = function() {
+      // Remove background when navigaging away
+      angular.element($document[0].body).css({
+        'background-image': 'none'
+      });
+
+      // Value of $scope.location can be found in tours' $stateParams
+      $state.go('tours', $scope.location);
+    };
   });
