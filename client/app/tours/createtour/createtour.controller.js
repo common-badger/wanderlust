f38ae5bef92a3a4188abd693d4262dec86ceafe3
Dan Thareja <danthareja@gmail.com>
2014-09-04 14:03:41 -0400

    (feat) adds wdlSpot directive

--- client/app/tours/createtour/createtour.controller.js
+++ client/app/tours/createtour/createtour.controller.js
@@ -2,5 +2,23 @@
 
 angular.module('wanderlustApp')
   .controller('CreatetourCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    var spotIndex = 0;
+    $scope.tour = {}; // Overall tour information
+    $scope.spots = [{}];
+
+    // var addSpot = function() {
+    //   spotCount++;
+    //   $scope.spots[spotCount] = {};
+    // };
+
+    $scope.createTour = function() {
+      console.log($scope.tour);
+      console.log($scope.spots);
+    };
+
+    // $scope.showNewSpot = function() {
+    //   addSpot();
+    // };
+
+
   });
--- client/app/tours/createtour/createtour.js
+++ client/app/tours/createtour/createtour.js
@@ -8,4 +8,13 @@ angular.module('wanderlustApp')
         templateUrl: 'app/tours/createtour/createtour.html',
         controller: 'CreatetourCtrl'
       });
+  })
+  .directive('wdlSpot', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        spot: '='
+      },
+      templateUrl: 'app/tours/createtour/wdlSpot.directive.html'
+    };
   });
