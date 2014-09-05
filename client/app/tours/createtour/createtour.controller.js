7cfb08aed83d5814be54cdbea1df271fc312e586
Dan Thareja <danthareja@gmail.com>
2014-09-05 18:57:13 -0400

    (feat) adds POST request to createTour()

--- client/app/tours/createtour/createtour.controller.js
+++ client/app/tours/createtour/createtour.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('wanderlustApp')
-  .controller('CreatetourCtrl', function ($scope) {
+  .controller('CreatetourCtrl', function ($scope, $http, Auth) {
     $scope.spots = [{}];
     $scope.tour = {spots: $scope.spots};
     $scope.neighborhoods = ['Bayview', 'Bernal Heights', 'Castro', 'Chinatown', 'Crocker Amazon', 'Dogpatch', 'Downtown', 'Excelsior', 'Financial District', 'Glen Park', 'Golden Gate Park', 'Haight-Ashbury', 'Inner Richmond', 'Inner Sunset', 'Lakeshore', 'Marina', 'Mission', 'Nob Hill', 'Noe Valley', 'North Beach', 'Ocean View', 'Outer Mission', 'Outer Richmond', 'Outer Sunset', 'Pacific Heights', 'Parkside', 'Portola', 'Potrero Hill', 'Russian Hill', 'South of Market', 'Tenderloin', 'Visitacion Valley', 'West of Twin Peaks', 'Western Addition'];
@@ -11,8 +11,14 @@ angular.module('wanderlustApp')
     };
 
     $scope.createTour = function() {
-      console.log($scope.tour);
-      console.log($scope.spots);
+      console.log("Trying to POST ", $scope.tour);
+      $http.post('/api/tours', $scope.tour)
+      .success(function(data) {
+        console.log("Post successful!");
+      })
+      .error(function(data) {
+        console.log("Error sending post request", data);
+      });
     };
 
   });
