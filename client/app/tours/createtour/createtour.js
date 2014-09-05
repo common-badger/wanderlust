c82289337dd29d5bda21f8e702fb3dc5349952d7
Dan Thareja <danthareja@gmail.com>
2014-09-05 17:08:33 -0400

    (feat) adds ability to upload spot pictures to imgur

--- client/app/tours/createtour/createtour.js
+++ client/app/tours/createtour/createtour.js
@@ -12,8 +12,34 @@ angular.module('wanderlustApp')
   .directive('wdlSpot', function() {
     return {
       restrict: 'E',
-      scope: {
-        spot: '='
+      controller: function($scope, $upload) {
+        $scope.onFileSelect = function($files) {
+          //$files: an array of files selected, each file has name, size, and type.
+          for (var i = 0; i < $files.length; i++) {
+            var file = $files[i];
+            var fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(file);
+            fileReader.onload = function(e) {
+              $upload.http({
+                url: 'https://api.imgur.com/3/image',
+                headers: {
+                  'Authorization': 'Client-ID 2b28684e6b6d23c',
+                  'Content-Type': file.type
+                },
+                data: e.target.result
+              }).progress(function(evt) {
+                console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+              }).success(function(data, status, headers, config) {
+                // file is uploaded successfully
+                console.log("upload successful!", data);
+                // Add data.link to $scope.spot[$index].imgurl
+                $scope.spot.imgurl = data.data.link;
+              }).error(function(data) {
+                console.error(data);
+              });
+            };
+          };
+        };
       },
       templateUrl: 'app/tours/createtour/wdlSpot.directive.html'
     };
