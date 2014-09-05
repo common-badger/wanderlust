cb5253c87c033ec095bcc6bcf63b737c61f02e52
Dan Thareja <danthareja@gmail.com>
2014-09-05 17:57:59 -0400

    (feat) adds more neighborhoods and cleans up styling

--- client/app/tours/createtour/createtour.controller.js
+++ client/app/tours/createtour/createtour.controller.js
@@ -3,10 +3,9 @@
 angular.module('wanderlustApp')
   .controller('CreatetourCtrl', function ($scope) {
     $scope.spots = [{}];
-    $scope.tour = {
-      spots: $scope.spots
-    };
-
+    $scope.tour = {spots: $scope.spots};
+    $scope.neighborhoods = ['Bayview', 'Bernal Heights', 'Castro', 'Chinatown', 'Crocker Amazon', 'Dogpatch', 'Downtown', 'Excelsior', 'Financial District', 'Glen Park', 'Golden Gate Park', 'Haight-Ashbury', 'Inner Richmond', 'Inner Sunset', 'Lakeshore', 'Marina', 'Mission', 'Nob Hill', 'Noe Valley', 'North Beach', 'Ocean View', 'Outer Mission', 'Outer Richmond', 'Outer Sunset', 'Pacific Heights', 'Parkside', 'Portola', 'Potrero Hill', 'Russian Hill', 'South of Market', 'Tenderloin', 'Visitacion Valley', 'West of Twin Peaks', 'Western Addition'];
+    
     $scope.addSpot = function() {
       $scope.spots.push({});
     };
