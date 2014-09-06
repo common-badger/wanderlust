6bb367a52a8ebc8982e98f29af997a11e4d6f428
Park Loqi Tamaroon <loqi@cruzio.com>
2014-09-06 09:40:08 -0700

    (cleanup) removes hard tabs from indentation; adds EOF newlines

--- server/api/tour/tour.model.spec.js
+++ server/api/tour/tour.model.spec.js
@@ -31,51 +31,51 @@ var tour = new Tour({
 
 describe('Tour Model',function(){
   beforeEach(function(done){
-  	Tour.remove().exec().then(function(){
-  	  done();
-  	});
+    Tour.remove().exec().then(function(){
+      done();
+    });
   });
 
   it('should save the data expected',function(done){
-  	tour.save(function(err,data){
-  	  should.not.exist(err);
-  	  should.exist(data);
-  	  done();
-  	})
+    tour.save(function(err,data){
+      should.not.exist(err);
+      should.exist(data);
+      done();
+    })
   });
 
   it('should not save data with title bigger than 50 char',function(done){
-  	var tourMax = new Tour({
-  	  title: 'avonawoevnapowiniruvirunipuraniuvnianvrvaowvanoiwnvepawnoievnauernvoinuviapwouaowpnvpowienv'
-  	});
-  	tourMax.save(function(err,data){
-  	  should.exist(err);
-  	  done();
-  	});
+    var tourMax = new Tour({
+      title: 'avonawoevnapowiniruvirunipuraniuvnianvrvaowvanoiwnvepawnoievnauernvoinuviapwouaowpnvpowienv'
+    });
+    tourMax.save(function(err,data){
+      should.exist(err);
+      done();
+    });
   });
 
 //come back to this test later
   // it('should fail when saving tour with same title',function(done){
-  // 	tour.save(function(err,data){
-  // 	  console.log('+++++++',data)
-  // 	  var tourDup = new Tour({title: 'The Mission Mission'});
-  // 	  tourDup.save(function(err,data){
-  // 	  	console.log('------',data);
-  // 	  	should.exist(err);
-  // 	  	done();
-  // 	  })
-  // 	})
+  //   tour.save(function(err,data){
+  //     console.log('+++++++',data)
+  //     var tourDup = new Tour({title: 'The Mission Mission'});
+  //     tourDup.save(function(err,data){
+  //       console.log('------',data);
+  //       should.exist(err);
+  //       done();
+  //     })
+  //   })
   // });
 
   it('should return the profile when called', function(done){
-  	tour.save(function(err,data){
-  	  data.profile.should.have.property('title');
-  	  data.profile.should.have.property('author');
-  	  data.profile.should.have.property('description');
-  	  data.profile.should.have.property('rating');
-  	  data.profile.should.have.property('duration');
-  	  done();
-  	});
+    tour.save(function(err,data){
+      data.profile.should.have.property('title');
+      data.profile.should.have.property('author');
+      data.profile.should.have.property('description');
+      data.profile.should.have.property('rating');
+      data.profile.should.have.property('duration');
+      done();
+    });
   });
 
   // it('should be able to add reviews', function(done){
