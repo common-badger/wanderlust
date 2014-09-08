4417dcbc96c528ab639acd7084eed02a2494d942
Dan Thareja <danthareja@gmail.com>
2014-09-08 11:15:17 -0700

    (cleanup) cleans up unnecessary injections

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('wanderlustApp')
-  .controller('MainCtrl', function ($scope, $state, $document, $location) {
+  .controller('MainCtrl', function ($scope, $state, $location) {
 
     $scope.navToToursByLocation = function() {
       // Value of $scope.location can be found in tours' $stateParams
