0f72c7d68b35f36312662c3a878be6667941422c
Dominic Czarnota <dczarno@gmail.com>
2014-09-08 10:54:28 -0700

    (inc) rebuilds tour display page

--- client/app/tours/tours.controller.js
+++ client/app/tours/tours.controller.js
@@ -3,45 +3,62 @@
 angular.module('wanderlustApp')
 
   .directive('starRating', function(){
-    return{
+    return {
       restrict: 'E',
       template: '<span class="glyphicon glyphicon-star"></span>'
     };
   })
 
   .directive('tagPrice', function(){
-    return{
+    return {
       restrict: 'E',
       template: '<span class="glyphicon glyphicon-usd"></span>'
     };
   })
 
   .directive('tagCamera', function(){
-    return{
+    return {
       restrict: 'E',
       template: '<span class="glyphicon glyphicon-camera"></span>'
     };
   })
 
   .directive('tagTree', function(){
-    return{
+    return {
       restrict: 'E',
       template: '<span class="glyphicon glyphicon-tree-conifer"></span>'
     };
   })
 
-  .controller('ToursCtrl', function ($scope, $location) {
+  .directive('tagDisplayer', function(){
+    httpGET.getData(function(data){
+      $scope.tours = data;
+      return $scopes.tours;    
+    });
+  })
 
-    //Temp tour list data
-    $scope.tours = {
-      name: 'The Mission Mission',
-      length: 'all day'
-    };
+  .factory('httpGET', function($http){
+    return {
+      getData: function(callback){
+        return $http({
+          method: 'GET',
+          url: '/api/tours'
+          }).success(function(data){
+            callback(data);
+          });
+      }
+    }
+  })
+
+  .controller('ToursCtrl', function ($scope, $location, $http, httpGET) {
+    
+    httpGET.getData(function(data){
+      $scope.tours = data;
+      console.log($scope.tours);
+    });
 
     //route to tour on click
     $scope.selectedTour = function(){
         $location.path('/tours/showtour');
     };
-
-
   });
