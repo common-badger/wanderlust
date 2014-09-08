462bd4eca1c9cac6b19521b327a501b4ec4d7806
Dominic Czarnota <dczarno@gmail.com>
2014-09-08 12:24:15 -0700

    (feat) adds tours from seed.js to tour display page

--- client/app/tours/tours.controller.js
+++ client/app/tours/tours.controller.js
@@ -30,35 +30,6 @@ angular.module('wanderlustApp')
     }
   })
 
-  .directive('tagDisplayer', function(){
-    httpGET.getData(function(data){
-      $scope.tours = data;
-      return $scopes.tours;    
-    });
-  })
-
-  // .directive('tourTemplate', function(){
-  //   return {
-  //     restrict: 'E',
-  //     template:
-  //      '<div class="tourView" ng-click="selectedTour()">
-  //         <img class="map" src="assets/images/SF_Mission_map.png" alt="Mission District">
-  //         <div class="tourViewInfo">
-  //           <span> {{tours.name}} </span></br>
-  //           <span> {{tours.length}} </span></br>
-  //           <star-Rating></star-Rating>
-  //           <star-Rating></star-Rating>
-  //           <star-Rating></star-Rating>
-  //           <star-Rating></star-Rating>
-  //           <star-Rating></star-Rating><br>
-  //           <tag-Price></tag-Price>
-  //           <tag-Camera></tag-Camera>
-  //           <tag-Tree></tag-Tree>
-  //         </div>
-  //       </div>'
-  //   }
-  // })
-
   .factory('httpGET', function($http){
     return {
       getData: function(callback){
@@ -76,7 +47,6 @@ angular.module('wanderlustApp')
     
     httpGET.getData(function(data){
       $scope.tours = data;
-      // $scope.title = $scope.tours.title;
       console.log($scope.tours);
     });
 
@@ -84,7 +54,4 @@ angular.module('wanderlustApp')
     $scope.selectedTour = function(){
         $location.path('/tours/showtour');
     };
-
-
-
   });
